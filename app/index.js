7cb2aad34426669e644dc5849a3e8d1050c50b06
Benjamin Otto <benjamin.otto@style-systems.de>
2016-05-19 14:27:09 +0200

    refactored app menu & added developer tools for "npm run start"

--- app/index.js
+++ app/index.js
@@ -32,13 +32,21 @@ app.on('ready', function() {
     mainWindow = null;
   });
 
-  var template = [{
-    label: "Application",
-    submenu: [
-      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-      { type: "separator" },
-      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-    ]}, {
+  var applicationMenu = {
+      label: "Application",
+      submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        {
+          label: "Quit", accelerator: "Command+Q",
+          click() {
+            app.quit();
+          }
+        }
+      ]
+    };
+
+  var editMenu = {
     label: "Edit",
     submenu: [
       { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
@@ -48,63 +56,71 @@ app.on('ready', function() {
       { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
       { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
       { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]},
-    {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Toggle Full Screen',
-          accelerator: process.platform === 'darwin' ? 'Ctrl+Command+F' : 'F11',
-          click(item, focusedWindow) {
-            if (focusedWindow)
-              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-          }
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-          click(item, focusedWindow) {
-            if (focusedWindow)
-              focusedWindow.webContents.toggleDevTools();
-          }
-        },
-      ]
-    },
-    {
-      label: 'Window',
-      role: 'window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'CmdOrCtrl+M',
-          role: 'minimize'
-        },
-        {
-          label: 'Close',
-          accelerator: 'CmdOrCtrl+W',
-          role: 'close'
-        },
-      ]
-    },
-    {
-      label: 'Help',
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click() { require('electron').shell.openExternal('https://github.com/Mercateo/dwatch'); }
-        },
-      ]
-    }
-  ];
+    ]
+  };
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  var viewMenu = {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Toggle Full Screen',
+        accelerator: process.platform === 'darwin' ? 'Ctrl+Command+F' : 'F11',
+        click(item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        }
+      }
+    ]
+  };
+
+  if (process.env.NODE_ENV === 'development') {
+    viewMenu.submenu.push({
+      label: 'Toggle Developer Tools',
+      accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+      click(item, focusedWindow) {
+        if (focusedWindow)
+          focusedWindow.webContents.toggleDevTools();
+      }
+    });
+  }
+
+  var windowMenu = {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      }
+    ]
+  };
+
+  var helpMenu = {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click() {
+          require('electron').shell.openExternal('https://github.com/Mercateo/dwatch');
+        }
+      }
+    ]
+  };
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate([ applicationMenu, editMenu, viewMenu, windowMenu, helpMenu ]));
 });
 
-function run(args, done) {
+function run (args, done) {
   const updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
   spawn(updateExe, args, {
     detached: true
   })
-  .on('close', done)
+    .on('close', done)
 }
